d094f768c0674d7b8f058c48a550306e0defb436
Stg8604 <131150429+Stg8604@users.noreply.github.com>
2024-02-15 10:23:01 +0000

    feat(ui): adding back button in leaderboard (#81)

--- src/pages/Leaderboard/LeaderBoard.tsx
+++ src/pages/Leaderboard/LeaderBoard.tsx
@@ -7,12 +7,14 @@ import { useEffect, useState } from "react";
 import leaderBg from "../../assets/Leaderboard/greenbg.svg";
 import header from "../../assets/Leaderboard/header.svg";
 import styles from "./styles.module.css";
+import { Button } from "@mantine/core";
+import { useNavigate } from "react-router-dom";
 
 const LeaderBoard = () => {
 	const dispatch = useAppDispatch();
 	const ranking = useAppSelector((state) => state.leaderboard.ranking);
 	const userName = useAppSelector((state) => state.status.userName);
-
+	const navigate = useNavigate();
 	const [page, setPage] = useState<number>(1);
 	const [pageData, setPageData] = useState<LeaderBoardResponse[]>([]);
 
@@ -43,6 +45,19 @@ const LeaderBoard = () => {
 						background: `url(${leaderBg}) no-repeat center center fixed`,
 					}}
 				>
+					<div
+						className={`${styles.image} absolute top-0 left-0 ml-4 mt-4 text-right mb-[1vh] border-2 border-solid font-pressStart2P bg-[#F5F5AC] shadow-[4px_4px_3px_1px_rgb(0,0,0,0.5)] p-[3px] rounded-3xl`}
+					>
+						<Button
+							onClick={() => {
+								navigate("/dashboard");
+							}}
+							className="text-black cursor-pointer"
+						>
+							Lobby
+						</Button>
+					</div>
+
 					<div
 						className={`${styles.heading} font-pressStart2P`}
 						style={{
